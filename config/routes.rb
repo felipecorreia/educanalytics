30d78f64e0880bdf1ec26369d87b6422e1505edb
Felipe <felipecorrei@gmail.com>
2013-11-24 02:19:22 -0200

    teste

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Educationanalytics::Application.routes.draw do
 
-root :to => "pages#home"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
